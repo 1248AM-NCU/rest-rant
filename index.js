4fa437ca995e59ad5f34b1dd42e1a5e52bcc6840
TheLegendary1248 <1248alex@gmail.com>
2022-05-09 20:35:24 -0400

    404 stuff whatever

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
+app.get('*', (req, res) => {
+    res.status(404).send('oh no')
+})
 
-app.listen(3000)
+app.listen(process.env.PORT)
