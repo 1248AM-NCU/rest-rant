3bb635ebc92f8de3cbf548cb3c50b2e1f371cbc7
TheLegendary1248 <1248alex+NCU@gmail.com>
2022-05-20 17:47:04 -0400

    Fancy styles

--- views/default.jsx
+++ views/default.jsx
@@ -8,6 +8,8 @@ function Def (html)
                 <title>
                     My very Title
                 </title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"/>
+                <link rel="stylesheet" href="/css/style.css"/>
             </head>
             <body>
                 <p>This is from Default</p>
