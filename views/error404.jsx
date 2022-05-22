2fc9b2f04905b57a3d9ea234a2210707b2576c5a
TheLegendary1248 <1248alex+NCU@gmail.com>
2022-05-22 09:33:14 -0400

    Added bootstrap stuffs

--- views/error404.jsx
+++ views/error404.jsx
@@ -1,17 +1,18 @@
 const React = require('react')
 const Def = require('./default')
 
-function error404 () {
-    return (
-      <Def>
-          <main>
-              <h1>404: PAGE NOT FOUND</h1>
-              <p>Oops, sorry, we can't find this page!</p>
-              <em>(however, it was probably totally your fault)</em>
-          </main>
-      </Def>
-    )
-  }
-  
+function error404() {
+  return (
+    <body style={{backgroundColor: "#0079d8"}}>
+      <main>
+        <div>
+          <img src="/images/BSoD.png" alt="BSoD" />
+        </div>
+        <div style={{fontFamily:"Arial", color:"white", position:"relative", left:"130px"}}>No, this page does not simply exist</div>
+      </main>
+    </body>
+  )
+}
+
 
 module.exports = error404
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home() {
     <Def>
       <main>
         <h1>HOME</h1>
+        <div>
+          <img src="/images/food.jpg" alt="Quiche?" />
+          <div>
+            Photo by <a href="https://www.flickr.com/photos/27312908@N05">Brenda Godinez</a> on <a href="https://wordpress.org/openverse/image/0ab85653-1c06-46aa-99b1-1070b1afe326/">Unsplash</a>
+          </div>
+        </div>
         <a href="/places">
           <button className="btn-primary">Places Page</button>
         </a>
