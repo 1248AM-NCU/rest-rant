ad0b2aec6e55837db58ea6bf5470a7cf7c3b4493
TheLegendary1248 <1248alex+NCU@gmail.com>
2022-05-23 20:55:06 -0400

    Nice form page

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -5,14 +5,33 @@ function New() {
         <Del>
             <main>
                 <h1>Create new place</h1>
-                <form>
-                    <label htmlFor="name">Restaurant Name</label>
-                    <input type="text" id="name"></input>
-                    <label htmlFor="year">Year Established</label>
-                    <input type="number" id="year"></input>
-                    <label htmlFor="img">Picture</label>
-                    <input type="image" id="img"></input>
-                    <input type="submit" value="Submit"></input>
+                <form method="POST" action="/places">
+                    <div className="form-group">
+                        <label htmlFor="name">Restaurant Name</label>
+                        <input type="text" id="name" required></input>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="year">Year Established</label>
+                        <input type="number" id="year"></input>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="img">Picture</label>
+                        <input type="url" id="img"></input>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="city">City</label>
+                        <input type="text" id="city"></input>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="state">State</label>
+                        <input type="text" id="state"></input>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="food">Cuisines</label>
+                        <input type="text" id="food" required></input>
+                    </div>
+                    <input className="btn btn-primary" type="submit" value="Submit"></input>
+
                 </form>
             </main>
         </Del>
