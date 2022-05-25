2f64e20beea679a6b390d0ae7229d31f65f38882
TheLegendary1248 <1248alex+NCU@gmail.com>
2022-05-25 06:55:40 -0400

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -9,10 +9,16 @@ function Def (html)
                     My very Title
                 </title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/>
-                <link rel="stylesheet" href="/css/style.css"/>
+                <link rel="stylesheet" href="/style.css"/>
             </head>
             <body>
-                <p>This is from Default</p>
+                <nav>
+                    <ul>
+                        <li><a href='/'>Home</a></li>
+                        <li><a href='/places'>Places</a></li>
+                        <li><a href='/places/new'>Add Place</a></li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
         </html>
