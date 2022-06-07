e9d2cc278c2f32b4ca1163357f6e09464f415f15
TheLegendary1248 <1248alex+NCU@gmail.com>
2022-06-06 23:18:17 -0400

    Part 10. there are bugs

--- controllers/places.js
+++ controllers/places.js
@@ -29,7 +29,9 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  .populate('comments')
   .then(place => {
+      console.log(place.comments)
       res.render('places/details', { place })
   })
   .catch(err => {
@@ -37,6 +39,7 @@ router.get('/:id', (req, res) => {
       res.render('error404')
   })
 })
+
 
 router.put('/:id', (req, res) => {
   res.send('PUT /places/:id stub')
@@ -50,8 +53,25 @@ router.get('/:id/edit', (req, res) => {
   res.send('GET edit form stub')
 })
 
-router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
+router.post('/:id/comment', (req, res) => {
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          console.log(err)
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
 })
 
 router.delete('/:id/rant/:rantId', (req, res) => {
--- views/places/details.jsx
+++ views/places/details.jsx
@@ -1,7 +1,26 @@
 const React = require("react")
 const Def = require("../default.jsx")
 
-function details(data) {
+function details({data}) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className="border">
+                    <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
             <h1>Place details</h1>
@@ -11,12 +30,32 @@ function details(data) {
             <h3>
                 {data.place.showEstablished()}
             </h3>
-            
             <h4>Serving {data.place.food}</h4>
+            <h2>Comments</h2>
+            {comments}
+            <form method="POST" action={`/places/${data.place._id}/comment`}>
+                <div className="rep">
+                    <label htmlFor="author">Your Name</label>
+                    <input type="text" id="author" name="author"></input>
+                </div>
+                <div className="rep">
+                    <label htmlFor="content">Comment</label>
+                    <input type="textarea" id="content" name="content"></input>
+                </div>
+                <div className="rep">
+                    <label htmlFor="stars">Star Rating</label>
+                    <input type="number" id="stars" name="stars" step="0.5" min="0" max="5"></input>
+                </div>
+                <div className="rep">
+                    <label htmlFor="rant">Rant!</label>
+                    <input type="checkbox" id="rant" name="rant" defaultChecked></input>
+                </div>
+                <button type="submit">Submit comment</button>
+            </form>
             <a href={`${data.index}/edit`} className="btn btn-warning">
                 Edit
             </a>
-            <form method="POST" action={`/places/${data.index}?_method=DELETE`}>
+            <form method="POST" action={`/places/${data.place._id}?_method=DELETE`}>
                 <button type="submit" className="btn btn-danger">
                     Delete
                 </button>
@@ -26,3 +65,4 @@ function details(data) {
 }
 
 module.exports = details
+
