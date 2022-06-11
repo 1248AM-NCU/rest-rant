2a81e75a379b19615ae28708f1de490c800f08b8
TheLegendary1248 <1248alex+NCU@gmail.com>
2022-06-11 08:13:35 -0400

    please tell me what's not working

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, 
   useUnifiedTopology: true
-})
+},
+() => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
 
 // Express Settings
 app.use(express.urlencoded({ extended: true }))
